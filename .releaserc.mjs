44cea4b45554aed8983b4895d99cceaa6170184f
Oleh Khavar <olehkhavar@gmail.com>
2026-01-30 22:54:01 +0200

    feat: add prettier

---
+++ .releaserc.mjs
@@ -0,0 +1,28 @@
+export default {
+  branches: ['main'],
+  tagFormat: 'v${version}',
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'major', release: 'major' },
+          { type: 'minor', release: 'minor' },
+          { type: 'feat', release: 'minor' },
+          { type: 'patch', release: 'patch' },
+          { type: 'fix', release: 'patch' },
+          { type: 'perf', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'revert', release: 'patch' },
+          { type: 'docs', release: false },
+          { type: 'chore', release: false },
+          { type: 'style', release: false },
+          { type: 'test', release: false },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+};
+
+export default config;
